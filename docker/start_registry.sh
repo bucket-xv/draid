bf1361ed4fbc671b1531adf22e0a10f1e8969998
Chenhao Xu <xv_chen_hao@163.com>
2025-03-15 11:51:07 +0800

    Fixing

--- docker/start_registry.sh
+++ docker/start_registry.sh
@@ -6,7 +6,7 @@ registry=$(tail -n 1 $DRAID_DIR/configs/int_ip_addrs_server.txt)
 
 sudo docker run -d -p 5000:5000 -p 5001:5001 --restart=always --name registry \
                 -v $DRAID_DIR/configs/registry.yml:/etc/docker/registry/config.yml \
-                registry
+                registry:2
 
                 # -v `pwd`/../configs/registry.yml:/etc/distribution/config.yml \
                 # -e REGISTRY_HTTP_ADDR=0.0.0.0:5000\
