a1cf7f4ffe2e47009bacb8502b3abe3514849a7f
Chenhao Xu <xv_chen_hao@163.com>
2025-03-15 10:00:44 +0800

    Fixing

--- deploy/deploy_source.sh
+++ deploy/deploy_source.sh
@@ -4,7 +4,7 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <num of osd servers> <osd disks>"
     exit 1
 fi
-./configure_host_source.sh
+./configure_host.sh
 ./configure_server.sh
 ./configure_cli.sh
-tools/deploy_service.sh $1 $2
+./deploy_service.sh $1 $2
