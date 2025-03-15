f9b13425f28eed4800ca485f3bd47d24bf6033fa
Chenhao Xu <xv_chen_hao@163.com>
2025-03-15 11:34:37 +0800

    Fixing

--- deploy/tools/gen_config.py
+++ deploy/tools/gen_config.py
@@ -10,13 +10,15 @@ if __name__ == '__main__':
     # Path to files
     project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     configs_dir = os.path.join(project_dir, 'configs')
+    ip_path = os.path.join(configs_dir, 'int_ip_addrs_server.txt')
     rgw_path = os.path.join(configs_dir, 'rgw.txt')
     user_path = os.path.join(configs_dir, 'user.json')
     template_path = os.path.join(project_dir, 'deploy', 'tools', 'template.yml')
     output_path = os.path.join(configs_dir, 'registry.yml')
 
     # First, get the urls of the rgw daemons
-
+    with open(ip_path, 'r') as f:
+        ips = f.readlines()
     with open(rgw_path, 'r') as f:
         daemons = f.read()
     lines = daemons.split('\n')
@@ -27,8 +29,7 @@ if __name__ == '__main__':
             break
         words = line.split()
         host = words[1]
-        id = int(host.split('.')[0][4:]) + 1
-        ip = '10.10.1.' + str(id)
+        ip = ips[-1]
         port = words[2].split(':')[1]
         url = 'http://' + ip + ':' + port
         urls.append(url)
