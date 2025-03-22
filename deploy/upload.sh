c952a7cccd7dc9e21fe08615692928f72212c7e6
Chenhao Xu <xv_chen_hao@163.com>
2025-03-22 14:39:58 +0800

    fixing

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -5,12 +5,14 @@ export DRAID_DIR=$(dirname "$(dirname $(realpath "$0"))")
 # Function to upload a certain image to the registry
 upload_image() {
     image_name=$1
+    tmp_file_name=/tmp/$(basename "$image_name").zip
     docker pull $image_name
-    docker save -o /tmp/$image_name.zip $image_name
+    echo "Saving $image_name to $tmp_file_name"
+    docker save -o $tmp_file_name $image_name
     export server_ip=$(head -n 1 $DRAID_DIR/configs/ip_addrs_all.txt)
     export registry_ip=$(head -n 1 $DRAID_DIR/configs/int_ip_addrs_server.txt)
-    scp /tmp/$image_name.zip $username@$server_ip:/tmp/$image_name.zip
-    ssh $username@$server_ip "sudo docker load -i /tmp/$image_name.zip"
+    scp $tmp_file_name $username@$server_ip:$tmp_file_name
+    ssh $username@$server_ip "sudo docker load -i $tmp_file_name"
     ssh $username@$server_ip "sudo docker tag $image_name $registry_ip:5000/$image_name"
     ssh $username@$server_ip "sudo docker push $registry_ip:5000/$image_name"
 }
