8a98c07bce057c3b08712cdac1d1b673e8a53e09
Chenhao Xu <xv_chen_hao@163.com>
2025-06-08 20:15:45 +0800

    Port change

--- docker/push_to_registry.sh
+++ docker/push_to_registry.sh
@@ -7,5 +7,5 @@ ip=$(tail -n 1 $DRAID_DIR/configs/int_ip_addrs_server.txt)
 echo $ip
 
 sudo docker pull $image
-sudo docker tag $image $ip:5000/$image
-sudo docker push $ip:5000/$image
+sudo docker tag $image $ip:5005/$image
+sudo docker push $ip:5005/$image
--- docker/start_registry.sh
+++ docker/start_registry.sh
@@ -4,6 +4,6 @@ export DRAID_DIR=$(dirname "$(dirname "$(realpath "$0")")")
 
 meta_registry=$(head -n 1 $DRAID_DIR/configs/int_ip_addrs_server.txt)
 
-sudo docker run -d -p 5000:5000 -p 5001:5001 --restart=always --name registry \
+sudo docker run -d -p 5005:5000 -p 5001:5001 --restart=always --name registry \
                 -v $DRAID_DIR/configs/registry.yml:/etc/docker/registry/config.yml \
                 $meta_registry:5000/registry:2
