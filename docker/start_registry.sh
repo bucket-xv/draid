b5cac14f9459c16d71a3202c153191757e689b4b
Chenhao Xu <xv_chen_hao@163.com>
2025-06-08 20:11:46 +0800

    Remove redundant

--- docker/start_registry.sh
+++ docker/start_registry.sh
@@ -7,9 +7,3 @@ meta_registry=$(head -n 1 $DRAID_DIR/configs/int_ip_addrs_server.txt)
 sudo docker run -d -p 5000:5000 -p 5001:5001 --restart=always --name registry \
                 -v $DRAID_DIR/configs/registry.yml:/etc/docker/registry/config.yml \
                 $meta_registry:5000/registry:2
-
-                # -v `pwd`/../configs/registry.yml:/etc/distribution/config.yml \
-                # -e REGISTRY_HTTP_ADDR=0.0.0.0:5000\
-
-# sudo docker tag ubuntu 10.10.1.1:5000/ubuntu:latest
-# sudo docker push 10.10.1.1:5000/ubuntu:latest
